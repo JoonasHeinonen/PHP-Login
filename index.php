a59b95277524096e9ba52ccbf0cd667906e6131a
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-07 22:24:08 +0300

    First commit.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="main-wrapper">
+                    <h1>HOMOJA! MEIDÄN KELLARISSA!?!<h1>
+                </div>
+            </nav>
+        </header>
+        <section>
+        </section>
+    </body>
+</html>
